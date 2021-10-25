a422bff2aaf5d3c317bc77f14ba092a6c0ddacf5
polk90 <92872300+polk90@users.noreply.github.com>
2021-10-25 19:14:37 +0300

    z4

--- main.py
+++ main.py
@@ -33,21 +33,47 @@ class BaseData(QMainWindow):
         for i in a:
             self.comboBox.addItem(str(i[0]))
         self.comboBox.show()
-        self.comboBox.activated[str].connect(self.onChanged)
+        self.comboBox.activated[str].connect(self.on_changed)
+        self.con.close()
 
-    def onChanged(self, text):
-        self.qlabel.setText(text)
-        self.qlabel.adjustSize(100, 100)
+    def on_changed(self, text):
+        self.write_que(text)
 
     def returned(self):
         self.hide()
         ex1 = MainMenuWidget(self)
         ex1.show()
 
+    def write_que(self, text):
+        self.hide()
+        f = WriteQue(self, text)
+        f.show()
+
+
+    def give_tb(self):
+        return self.table_n
+
 
 class WriteQue(QMainWindow):
-    pass
+    def __init__(self, parent=None, table=None):
+        super(WriteQue, self).__init__(parent)
+        self.table_name = table
+        uic.loadUi('Add_Que.ui', self)
+        self.pushButton.clicked.connect(self.save_button)
 
+    def save_button(self):
+        text = self.plainTextEdit.toPlainText()
+        self.con = sqlite3.connect("PhB.db")
+        cur = self.con.cursor()
+        idd = cur.execute('select count(*) from ' + self.table_name).fetchone()[0] + 1
+        to_execute = self.table_name + '(id, name) VALUES(' + str(idd) + ',' + '"' + text + '")'
+        a = 'INSERT INTO ' + to_execute
+        cur.execute(a)
+        self.con.commit()
+        self.con.close()
+        self.close()
+        ex = BaseData(self)
+        ex.show()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainMenuWidget()

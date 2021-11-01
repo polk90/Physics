bfac4718d120d1f5b2d9ddea3b948fc8577659e6
polk90 <92872300+polk90@users.noreply.github.com>
2021-11-01 19:18:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QPushButton, QLineEdit, QInputDialog
 import sqlite3
 
 
@@ -10,10 +10,16 @@ class MainMenuWidget(QMainWindow):
         super(MainMenuWidget, self).__init__(parent)
         uic.loadUi('Main_Menu.ui', self)
         self.pushButton_2.clicked.connect(self.open_base_data)
+        self.pushButton.clicked.connect(self.open_generate_work)
 
     def open_base_data(self):
         f1 = BaseData(self)
         f1.show()
+        self.hide()
+
+    def open_generate_work(self):
+        f = CreateWork(self)
+        f.show()
         self.hide()
 
 
@@ -23,6 +29,7 @@ class BaseData(QMainWindow):
         uic.loadUi('Base_Data.ui', self)
         self.pushButton_2.clicked.connect(self.returned)
         self.pushButton.clicked.connect(self.ex4)
+        self.pushButton_3.clicked.connect(self.add_theme)
         self.comboBox.hide()
 
     def ex4(self):
@@ -33,47 +40,135 @@ class BaseData(QMainWindow):
         for i in a:
             self.comboBox.addItem(str(i[0]))
         self.comboBox.show()
-        self.comboBox.activated[str].connect(self.on_changed)
+        self.comboBox.activated[str].connect(self.onChanged)
         self.con.close()
 
-    def on_changed(self, text):
-        self.write_que(text)
+    def onChanged(self, text):
+        self.hide()
+        a = WriteQue(self, text)
+        a.show()
 
     def returned(self):
         self.hide()
         ex1 = MainMenuWidget(self)
         ex1.show()
 
-    def write_que(self, text):
-        self.hide()
-        f = WriteQue(self, text)
-        f.show()
-
-
-    def give_tb(self):
-        return self.table_n
+    def add_theme(self):
+        text, ok = QInputDialog.getText(self, 'Добавить раздел',
+                                        'Введите название')
+        if ok:
+            con = sqlite3.connect("PhB.db")
+            cur = con.cursor()
+            a = "CREATE TABLE " + text + "(body STRING NOT NULL);"
+            cur.execute(a)
+            con.commit()
+            con.close()
 
 
 class WriteQue(QMainWindow):
-    def __init__(self, parent=None, table=None):
-        super(WriteQue, self).__init__(parent)
-        self.table_name = table
-        uic.loadUi('Add_Que.ui', self)
-        self.pushButton.clicked.connect(self.save_button)
+    def __init__(self, parent=None, text=None):
+        super(QMainWindow, self).__init__(parent)
+        uic.loadUi('Write_Que.ui', self)
+        self.text = text
+        self.pushButton.clicked.connect(self.save_result)
+        self.spinBox.hide()
+        self.spinBox_2.hide()
+        self.spinBox_3.hide()
+        self.spinBox_4.hide()
+        self.spinBox_5.hide()
+        self.spinBox_6.hide()
+        self.spinBox_7.hide()
+        self.spinBox_8.hide()
+        self.spinBox_9.hide()
+        self.spinBox_10.hide()
+        self.label_1.hide()
+        self.label_2.hide()
+        self.label_3.hide()
+        self.label_4.hide()
+        self.label_5.hide()
+        self.label_6.hide()
+        self.label_7.hide()
+        self.label_8.hide()
+        self.label_9.hide()
+        self.label_10.hide()
+        self.label_11.hide()
+        self.label_12.hide()
+        self.label_13.hide()
+        self.label_14.hide()
+        self.label_15.hide()
+        self.pushButton_2.clicked.connect(self.ready)
 
-    def save_button(self):
-        text = self.plainTextEdit.toPlainText()
-        self.con = sqlite3.connect("PhB.db")
-        cur = self.con.cursor()
-        idd = cur.execute('select count(*) from ' + self.table_name).fetchone()[0] + 1
-        to_execute = self.table_name + '(id, name) VALUES(' + str(idd) + ',' + '"' + text + '")'
-        a = 'INSERT INTO ' + to_execute
+    def save_result(self):
+        con = sqlite3.connect("PhB.db")
+        cur = con.cursor()
+        a = "INSERT INTO " + self.text + "(body) VALUES(" + '"'\
+            + self.plainTextEdit.toPlainText() + '"' + ')'
         cur.execute(a)
-        self.con.commit()
-        self.con.close()
+        con.commit()
+        con.close()
         self.close()
-        ex = BaseData(self)
-        ex.show()
+        a = MainMenuWidget(self)
+        a.show()
+
+    def activ_1(self):
+        self.spinBox.show()
+        self.spinBox_2.show()
+        self.label_1.show()
+        self.label_2.show()
+        self.label_3.show()
+
+    def activ_2(self):
+        self.activ_1()
+        self.spinBox_3.show()
+        self.spinBox_4.show()
+        self.label_4.show()
+        self.label_5.show()
+        self.label_6.show()
+
+    def activ_3(self):
+        self.activ_2()
+        self.spinBox_5.show()
+        self.spinBox_6.show()
+        self.label_7.show()
+        self.label_8.show()
+        self.label_9.show()
+    def activ_4(self):
+        self.activ_3()
+        self.spinBox_7.show()
+        self.spinBox_8.show()
+        self.label_7.show()
+        self.label_8.show()
+        self.label_10.show()
+        self.label_11.show()
+        self.label_12.show()
+
+    def activ_5(self):
+        self.activ_4()
+        self.spinBox_9.show()
+        self.spinBox_10.show()
+        self.label_13.show()
+        self.label_14.show()
+        self.label_15.show()
+
+    def ready(self):
+        text = self.plainTextEdit.toPlainText()
+        if text.count('!') == 5:
+            self.activ_5()
+        elif text.count('!') == 4:
+            self.activ_4()
+        elif text.count('!') == 3:
+            self.activ_3()
+        elif text.count('!') == 2:
+            self.activ_2()
+        elif text.count('!') == 1:
+            self.activ_1()
+
+class CreateWork(QMainWindow):
+    def __init__(self, parent=None):
+        super(CreateWork, self).__init__(parent)
+        uic.loadUi('Generate_Work.ui', self)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainMenuWidget()
